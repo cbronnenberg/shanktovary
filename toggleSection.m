7cacc8692ea1e43182a1bceba0d092e1ee1b2efc
Chris Bronnenberg <c.bronnenberg@yahoo.com>
2026-02-14 22:44:47 -0800

    updated toggle for App Designer bug not minimize a 'fit' row.

--- toggleSection.m
+++ toggleSection.m
@@ -1,9 +1,21 @@
-function toggleSection(app, contentPanel, headerButton)
+function toggleSection(app, contentPanel, headerButton, rowIndex)
     if strcmp(contentPanel.Visible,'on')
+        % Collapse
         contentPanel.Visible = 'off';
         headerButton.Text = strrep(headerButton.Text, "▼", "►");
+
+        % Shrink the row
+        app.LeftGrid.RowHeight{rowIndex} = 'fit';
+
     else
+        % Expand
         contentPanel.Visible = 'on';
         headerButton.Text = strrep(headerButton.Text, "►", "▼");
+
+        % Let the row auto-size to content
+        app.LeftGrid.RowHeight{rowIndex} = 'fit';
     end
 end
+
+% Usage
+% app.toggleSection(app.AccelContentPanel, app.AccelHeaderButton, 1)
